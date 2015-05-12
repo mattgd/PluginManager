c75f2e72bc87bb12dd73e9e873e14783b3fd3e4b
lvletei <revilution123@gmail.com>
2015-05-11 23:57:37 -0600

    Beginning localization stuff

--- src/main/java/com/jadeningle/PluginManager/PluginManagerReloaded.java
+++ src/main/java/com/jadeningle/PluginManager/PluginManagerReloaded.java
@@ -1,19 +1,45 @@
 package com.jadeningle.PluginManager;
+
+import java.io.File;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginManagerReloaded extends JavaPlugin {
 	
+	public FileConfiguration language;
 	@Override
 	public void onEnable( ) 
 	{
-		getCommand( "plm" ).setExecutor( new pluginManagerCommand( this ) );
-		getLogger().info("Plugin Manager Reloaded is now enabled!");
+		getCommand( "pluginmanager" ).setExecutor( new pluginManagerCommand( this ) );
+		initConfig( );
+		getLogger( ).info("Plugin Manager Reloaded is now enabled!");
 	}
 	
 	@Override
 	public void onDisable( ) 
 	{
-		getLogger().info("Plugin Manager Reloaded is now disabled!");
+		getLogger( ).info("Plugin Manager Reloaded is now disabled!");
 	}
-
+	
+	private void initConfig( )
+	{
+		FileConfiguration config = getConfig( );
+		if( !new File( getDataFolder( ), "config.yml").exists( ) ) 
+		{
+			config.addDefault( "Updater.Notify", true );
+			config.addDefault( "Language", "en_US" );
+			config.addDefault( "ConfigVersion", 0.1);
+			config.addDefault( "Debug", false );
+			config.options( ).copyDefaults( true );
+			saveConfig( );
+		}
+		
+		String[] langArray = {"en_US"};
+		for( String string : langArray )
+			saveResource( "locale" + File.separator + string + ".yml", false );
+		
+		language = YamlConfiguration.loadConfiguration(  new File( getDataFolder( ) + File.separator + "locale", this.getConfig( ).getString( "Language" ) + ".yml") );
+	}
+	
 }
--- src/main/java/com/jadeningle/PluginManager/pluginManagerCommand.java
+++ src/main/java/com/jadeningle/PluginManager/pluginManagerCommand.java
@@ -4,26 +4,25 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class pluginManagerCommand implements CommandExecutor 
 {
 	private final PluginManagerReloaded plugin;
 	
-	public pluginManagerCommand( PluginManagerReloaded pluginManagerReloaded )
+	public pluginManagerCommand( PluginManagerReloaded instance )
 	{
-		plugin = pluginManagerReloaded;
+		plugin = instance;
 	}
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] split)
     {
-    	Player player = (Player) sender;
     	
     	if( split.length == 0 )
         {
-        	player.sendMessage( ChatColor.GREEN + "|------------------" + ChatColor.GRAY + "PluginManagerReloaded Help" + ChatColor.GREEN + "------------------|" );
-        	player.sendMessage( ChatColor.GOLD + "/plm help <command> "+ ChatColor.AQUA +"- Command help" );
-        		
+    		String[] listArray = {"Help", "Enable", "Disable", "Load", "Unload", "Reload", "Sreload", "Show", "List"};
+	        sender.sendMessage( ChatColor.GREEN + "|------------------" + ChatColor.GRAY + "PluginManager Help" + ChatColor.GREEN + "------------------|" );
+	        for( String string : listArray )
+	        	sender.sendMessage( ChatColor.GOLD + "/" + label + " " + ChatColor.translateAlternateColorCodes( '&', plugin.language.getString( "Command.Description." + string ) ) );
         	return true;
         }
     	return false;
